aeda4d116153254a0bafa585b3a9f87eaee2c826
XxRexRaptorxX <Raptor-Simon@web.de>
2022-02-02 00:53:46 +0100

    changed fluid attributes

--- src/main/java/xxrexraptorxx/minetraps/fluids/FluidToxin.java
+++ src/main/java/xxrexraptorxx/minetraps/fluids/FluidToxin.java
@@ -15,8 +15,7 @@ public class FluidToxin {
 
     public static final ForgeFlowingFluid.Properties PROPERTIES = new ForgeFlowingFluid.Properties(ModFluids.TOXIN, ModFluids.FLOWING_TOXIN,
                 FluidAttributes.builder(new ResourceLocation(References.MODID, "block/toxin_still"), new ResourceLocation(References.MODID, "block/toxin_flow"))
-                        .sound(SoundEvents.BUCKET_FILL, SoundEvents.BUCKET_EMPTY).luminosity(15)).block(() -> (LiquidBlock) ModBlocks.TOXIN.get()).bucket(ModItems.TOXIN_BUCKET);
+                        .sound(SoundEvents.BUCKET_FILL, SoundEvents.BUCKET_EMPTY).viscosity(3000).density(2500)).block(() -> (LiquidBlock) ModBlocks.TOXIN.get()).bucket(ModItems.TOXIN_BUCKET);
 
         private FluidToxin() {}
-
 }

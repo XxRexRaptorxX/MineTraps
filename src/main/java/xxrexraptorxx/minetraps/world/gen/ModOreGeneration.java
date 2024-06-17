db3a8663807eed30b274ba4f838f3b82c9eff3ae
Pete MC-Mods <mcmods.pete@gmail.com>
2024-06-18 00:34:26 +0200

    small change so that Quicksand only generates in Jungles and Swamps

--- src/main/java/xxrexraptorxx/minetraps/world/gen/ModOreGeneration.java
+++ src/main/java/xxrexraptorxx/minetraps/world/gen/ModOreGeneration.java
@@ -2,12 +2,22 @@ package xxrexraptorxx.minetraps.world.gen;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.structure.JungleTempleStructure;
 import xxrexraptorxx.minetraps.world.MineTrapsPlacedFeatures;
+
+import java.util.Set;
 
 public class ModOreGeneration {
     public static void generateOres() {
-        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
+        Set<RegistryKey<Biome>> immutable = Set.of(BiomeKeys.JUNGLE, BiomeKeys.MANGROVE_SWAMP, BiomeKeys.BAMBOO_JUNGLE, BiomeKeys.SPARSE_JUNGLE);
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(immutable),
                 GenerationStep.Feature.TOP_LAYER_MODIFICATION, MineTrapsPlacedFeatures.QUICKSAND_PLACED_KEY);
     }
 }

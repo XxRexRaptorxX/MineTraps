b4232293e9d23f8f18629ffb395408e2b4b53a08
Pete MC-Mods <mcmods.pete@gmail.com>
2024-10-15 00:00:31 +0200

    backport of gradle changes and other fixes

--- src/main/java/xxrexraptorxx/minetraps/main/References.java
+++ src/main/java/xxrexraptorxx/minetraps/main/References.java
@@ -1,5 +1,10 @@
 package xxrexraptorxx.minetraps.main;
 
+
+/**
+ * @author XxRexRaptorxX (RexRaptor), MC Mods Pete [>Fabric Port]
+ * @projectPage https://www.curseforge.com/minecraft/mc-mods/minetraps-fabric
+ **/
 public class References {
 
     public static final String NAME = "MineTraps";

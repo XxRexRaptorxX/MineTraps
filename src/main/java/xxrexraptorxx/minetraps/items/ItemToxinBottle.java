292dd7f2b178cf563e6baa800e6729f16ff3ef7e
Pete MC-Mods <mcmods.pete@gmail.com>
2025-01-21 22:08:25 +0100

    fix toxin bottle crash

--- src/main/java/xxrexraptorxx/minetraps/items/ItemToxinBottle.java
+++ src/main/java/xxrexraptorxx/minetraps/items/ItemToxinBottle.java
@@ -13,6 +13,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public class ItemToxinBottle extends Item {
 
@@ -26,20 +27,34 @@ public class ItemToxinBottle extends Item {
 
 
     @Override
-    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pEntityLiving) {
+    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack pStack, @NotNull Level pLevel, @NotNull LivingEntity pEntityLiving) {
         super.finishUsingItem(pStack, pLevel, pEntityLiving);
-            ServerPlayer serverplayer = (ServerPlayer) pEntityLiving;
-            CriteriaTriggers.CONSUME_ITEM.trigger(serverplayer, pStack);
-            pStack.shrink(1);
-            serverplayer.awardStat(Stats.ITEM_USED.get(this));
-
+        if (pEntityLiving instanceof ServerPlayer serverPlayer) {
+            CriteriaTriggers.CONSUME_ITEM.trigger(serverPlayer, pStack);
+            serverPlayer.awardStat(Stats.ITEM_USED.get(this));
+        }
 
         if (!pLevel.isClientSide) {
             pEntityLiving.addEffect(new MobEffectInstance(MobEffects.POISON, 800, 1));
             pEntityLiving.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 400, 0));
         }
-            return new ItemStack(Items.GLASS_BOTTLE);
+
+        if (pEntityLiving instanceof Player playerEntity) {
+            if (!playerEntity.isCreative()) {
+                pStack.shrink(1);
+                if (!pStack.isEmpty()) {
+                    ItemStack itemStack = new ItemStack(Items.GLASS_BOTTLE);
+                    if (!playerEntity.getInventory().add(itemStack)) {
+                        playerEntity.drop(itemStack, false);
+                    }
+                } else {
+                    return new ItemStack(Items.GLASS_BOTTLE);
+                }
+            }
+        }
+        return pStack;
     }
+
 
     @Override
     public int getUseDuration(ItemStack stack, LivingEntity entity) {
